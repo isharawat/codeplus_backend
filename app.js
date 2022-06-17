076b45e5982b20b70689f132043778aee7c934d9
ISHA <isharawat2142@gmail.com>
2022-06-17 13:01:26 +0530

    added

---
+++ app.js
@@ -0,0 +1,195 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const port = 3001;
+const dotenv= require('dotenv');
+dotenv.config({path: './config.env'});
+require('./db/conn');
+
+const Post = require('./models/Post');
+// require model
+const User = require('./models/userSchema');
+
+const Question = require('./models/Question');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+// Registration
+app.post('./register',(req,res)=>{
+    try{
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+        const createUser= new User({
+            username : username,
+            email : email,
+            password : password
+        });
+        
+        res.status(200).send("Registered");
+    }
+    catch(error){
+        res.status(400).send(error)
+    }
+})
+
+
+
+
+
+
+// For posts
+
+app.post('/add-posts', async(req,res) => {
+ 
+  const newpost = new Post(req.body);
+  try{
+      await newpost.save()
+      res.status(201).json({
+          status: 'Success',
+          data : {
+              newpost
+          }
+      })
+  }catch(err){
+      res.status(500).json({
+          status: 'Failed',
+          message : err
+      })
+  }
+})
+
+app.get('/get-posts', async(req,res) => {
+  const posts = await Post.find({})
+  try{
+      res.status(201).json({
+          status: 'Success',
+          data : {
+              posts
+          }
+      })
+  }catch(err){
+      res.status(500).json({
+          status: 'Failed',
+          message : err
+      })
+  }
+})
+
+app.patch('/update-post/:id', async (req,res) => {
+  const updatedposts = await Post.findByIdAndUpdate(req.params.id,req.body,{
+      new : true,
+      runValidators : true
+    })
+  try{
+      res.status(200).json({
+          status : 'Success',
+          data : {
+            updatedposts
+          }
+        })
+  }catch(err){
+      console.log(err)
+  }
+})
+
+app.delete('/delete-post/:id', async(req,res) => {
+  await Post.findByIdAndDelete(req.params.id)
+  
+  try{
+    res.status(204).json({
+        status : 'Success',
+        data : {}
+    })
+  }catch(err){
+      res.status(500).json({
+          status: 'Failed',
+          message : err
+      })
+  }
+})
+
+//Questions
+
+app.post('/add-question', async(req,res) => {
+  
+  const newquestion = new Question(req.body)
+  try{
+      await newquestion.save()
+      res.status(201).json({
+          status: 'Success',
+          data : {
+              newquestion
+          }
+      })
+  }catch(err){
+      res.status(500).json({
+          status: 'Failed',
+          message : err
+      })
+  }
+})
+
+app.get('/get-questions', async (req,res) => {
+  const questions= await Question.find({})
+  try{
+    res.status(201).json({
+        status: 'Success',
+        data : {
+            questions
+        }
+    })
+}catch(err){
+    res.status(500).json({
+        status: 'Failed',
+        message : err
+    })
+}
+})
+
+app.patch('/update-question/:id', async (req,res) => {
+    const updatedquestions = await Question.findByIdAndUpdate(req.params.id,req.body,{
+        new : true,
+        runValidators : true
+      })
+    try{
+        res.status(200).json({
+            status : 'Success',
+            data : {
+              updatedquestions
+            }
+          })
+    }catch(err){
+        console.log(err)
+    }
+})
+
+
+app.delete('/delete-question/:id', async(req,res) => {
+  await Question.findByIdAndDelete(req.params.id)
+  
+  try{
+    res.status(204).json({
+        status : 'Success',
+        data : {}
+    })
+  }catch(err){
+      res.status(500).json({
+          status: 'Failed',
+          message : err
+      })
+  }
+})
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ models/Question.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const questionSchema = new mongoose.Schema({
+    question: {
+        type : String,
+        required : true
+    },
+    points: {
+        type : Number,
+        required : true
+    }
+})
+
+const Question = mongoose.model('Question',questionSchema)
+
+module.exports = Question
